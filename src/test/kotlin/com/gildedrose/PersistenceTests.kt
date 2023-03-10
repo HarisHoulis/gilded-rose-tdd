1a39b0544e360ae602ce32a1057487c664b1ef5b
houlisha <16525973+HarisHoulis@users.noreply.github.com>
2023-03-10 09:41:19 +0100

    Load new file format and handle errors

--- src/test/kotlin/com/gildedrose/PersistenceTests.kt
+++ src/test/kotlin/com/gildedrose/PersistenceTests.kt
@@ -1,32 +1,90 @@
 package com.gildedrose
 
-import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.fail
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.io.TempDir
 import java.io.File
+import java.io.IOException
+import java.time.Instant
 
 class PersistenceTests {
 
+    private val now = Instant.now()
+
+    private val items = listOf(
+        Item("banana", march1, 42u),
+        Item("peach", march1.plusDays(1), 100u),
+    )
+
     @Test
-    fun `load and save`(@TempDir dir: File) {
+    fun `save and load`(@TempDir dir: File) {
         val file = File(dir, "stock.tsv")
-        val stock = listOf(
-            Item("banana", march1, 42u),
-            Item("peach", march1.plusDays(1), 100u),
+
+        val stockList = StockList(now, items)
+
+        stockList.saveTo(file)
+
+        assertEquals(
+            stockList,
+            file.loadItems(defaultLastModified = now.plusSeconds(3600))
         )
-
-        stock.saveTo(file)
-
-        file.loadItems() shouldBe stock
     }
 
     @Test
-    fun `load and save empty`(@TempDir dir: File) {
+    fun `save and load empty`() {
+        val stockList = StockList(now, emptyList())
+
+        assertEquals(
+            stockList,
+            stockList.toLines().toStockList(defaultLastModified = now.plusSeconds(3600))
+        )
+    }
+
+    @Test
+    fun `load with no last modified date`() {
+        val lines = sequenceOf("# Banana")
+
+        assertEquals(
+            StockList(now, emptyList()),
+            lines.toStockList(defaultLastModified = now)
+        )
+    }
+
+    @Test
+    fun `load with blank last modified date`() {
+        val lines = sequenceOf("# LastModified:")
+
+        try {
+            lines.toStockList(defaultLastModified = now)
+            fail("didn't throw")
+        } catch (e: IOException) {
+            assertEquals(
+                "Could not parse LastModified header: Text '' could not be parsed at index 0",
+                e.message
+            )
+        }
+    }
+
+    @Test
+    fun `load legacy file`(@TempDir dir: File) {
         val file = File(dir, "stock.tsv")
-        val stock = emptyList<Item>()
 
-        stock.saveTo(file)
+        items.legacySaveTo(file)
 
-        file.loadItems() shouldBe stock
+        assertEquals(
+            StockList(now, items),
+            file.loadItems(now)
+        )
+    }
+
+}
+
+private fun List<Item>.legacySaveTo(file: File) {
+    fun Item.toLine() = "$name\t$sellByDate\t$quality"
+    file.writer().buffered().use { writer ->
+        forEach { item ->
+            writer.appendLine(item.toLine())
+        }
     }
 }
