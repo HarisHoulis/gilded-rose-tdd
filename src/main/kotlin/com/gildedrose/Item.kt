d5263ef1e90cae052b3cd14ecf5c6bfcfe4213a0
Haris Houlis <16525973+HarisHoulis@users.noreply.github.com>
2023-07-18 08:09:06 +0200

    Reduce quality by 2 after sell-by date

--- src/main/kotlin/com/gildedrose/Item.kt
+++ src/main/kotlin/com/gildedrose/Item.kt
@@ -13,7 +13,17 @@ data class Item(
         }
     }
 
-    fun updatedBy(days: Int) =
-        copy(quality = (quality - days).coerceAtLeast(0))
+    fun updatedBy(days: Int, on: LocalDate): Item {
+        val dates = (1 - days..0).map { on.plusDays(it.toLong()) }
+        dates.forEach { println(it) }
+        return dates.fold(this) { item, date ->
+            item.update(date)
+        }
+    }
+
+    private fun update(on: LocalDate): Item {
+        val degradation = if (on.isAfter(sellByDate)) 2 else 1
+        return copy(quality = (quality - degradation).coerceAtLeast(0))
+    }
 
 }
