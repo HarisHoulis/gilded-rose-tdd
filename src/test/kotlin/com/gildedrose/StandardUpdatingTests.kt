50b685c8c344ddbe2ecd2a9e3570eababee11fa9
houlisha <16525973+HarisHoulis@users.noreply.github.com>
2023-07-31 09:08:08 +0200

    Extract AgedBrieUpdatingTests

--- src/test/kotlin/com/gildedrose/StandardUpdatingTests.kt
+++ src/test/kotlin/com/gildedrose/StandardUpdatingTests.kt
@@ -3,7 +3,7 @@ package com.gildedrose
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
-internal class UpdatingTests {
+internal class StandardUpdatingTests {
 
     @Test
     fun `items decrease in quality one per day`() {
@@ -58,34 +58,6 @@ internal class UpdatingTests {
         assertEquals(
             Item("banana", null, 42),
             Item("banana", null, 42).updatedBy(1, on = march1.plusDays(1))
-        )
-    }
-
-    @Test
-    fun `Aged Brie increases in quality by one every day until its sell-by date`() {
-        assertEquals(
-            Item("Aged Brie", march1, 43),
-            Item("Aged Brie", march1, 42).updatedBy(1, on = march1)
-        )
-    }
-
-    @Test
-    fun `Aged Brie increases in quality by two every day after its sell-by date`() {
-        assertEquals(
-            Item("Aged Brie", march1, 44),
-            Item("Aged Brie", march1, 42).updatedBy(1, on = march1.plusDays(1))
-        )
-    }
-
-    @Test
-    fun `Aged Brie doesn't get better than 50`() {
-        assertEquals(
-            Item("Aged Brie", march1, 50),
-            Item("Aged Brie", march1, 50).updatedBy(1, on = march1)
-        )
-        assertEquals(
-            Item("Aged Brie", march1, 50),
-            Item("Aged Brie", march1, 49).updatedBy(1, on = march1.plusDays(1))
         )
     }
 }
