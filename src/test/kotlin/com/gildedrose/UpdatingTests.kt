a9955360f8e90976309977945c1ab8741ad423f3
houlisha <16525973+HarisHoulis@users.noreply.github.com>
2023-07-31 08:38:57 +0200

    Implement new Aged Brie Item

--- src/test/kotlin/com/gildedrose/UpdatingTests.kt
+++ src/test/kotlin/com/gildedrose/UpdatingTests.kt
@@ -60,4 +60,32 @@ internal class UpdatingTests {
             Item("banana", null, 42).updatedBy(1, on = march1.plusDays(1))
         )
     }
+
+    @Test
+    fun `Aged Brie increases in quality by one every day until its sell-by date`() {
+        assertEquals(
+            Item("Aged Brie", march1, 43),
+            Item("Aged Brie", march1, 42).updatedBy(1, on = march1)
+        )
+    }
+
+    @Test
+    fun `Aged Brie increases in quality by two every day after its sell-by date`() {
+        assertEquals(
+            Item("Aged Brie", march1, 44),
+            Item("Aged Brie", march1, 42).updatedBy(1, on = march1.plusDays(1))
+        )
+    }
+
+    @Test
+    fun `Aged Brie doesn't get better than 50`() {
+        assertEquals(
+            Item("Aged Brie", march1, 50),
+            Item("Aged Brie", march1, 50).updatedBy(1, on = march1)
+        )
+        assertEquals(
+            Item("Aged Brie", march1, 50),
+            Item("Aged Brie", march1, 49).updatedBy(1, on = march1.plusDays(1))
+        )
+    }
 }
