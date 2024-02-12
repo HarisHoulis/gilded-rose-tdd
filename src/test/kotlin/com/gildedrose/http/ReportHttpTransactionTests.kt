4aa46a1e62dbd68afcc1cb9979e86dca32189a68
houlisha <16525973+HarisHoulis@users.noreply.github.com>
2024-02-12 08:34:21 +0100

    Add utility assertAll fun

--- src/test/kotlin/com/gildedrose/http/ReportHttpTransactionTests.kt
+++ src/test/kotlin/com/gildedrose/http/ReportHttpTransactionTests.kt
@@ -1,14 +1,14 @@
 package com.gildedrose.http
 
+import com.gildedrose.com.gildedrose.testing.assertAll
 import org.http4k.core.Method
 import org.http4k.core.Request
 import org.http4k.core.Response
 import org.http4k.core.Status
 import org.http4k.core.then
-import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.assertAll
 import java.time.Duration
+import kotlin.test.assertEquals
 
 internal class ReportHttpTransactionTests {
 
@@ -22,37 +22,31 @@ internal class ReportHttpTransactionTests {
         filter.then {
             Response(Status.OK)
         }.invoke(Request(Method.GET, "/"))
-
-        with(events.single() as HttpEvent) {
-            assertAll(
-                { assertEquals("/", uri) },
-                { assertEquals(Method.GET.toString(), method) },
-                { assertEquals(Status.OK.code, status) }
-            )
-        }
+        assertAll(
+            events.single() as HttpEvent,
+            { assertEquals("/", uri) },
+            { assertEquals(Method.GET.toString(), method) },
+            { assertEquals(Status.OK.code, status) }
+        )
     }
 
     @Test
-    fun `reports slow transactions to analytics`() {
+    fun `reports slow transactions to analytics with an additional event`() {
         filter.then {
             Thread.sleep(25)
             Response(Status.OK)
         }.invoke(Request(Method.GET, "/"))
-
-        with(events.first() as HttpEvent) {
-            assertAll(
-                { assertEquals("/", uri) },
-                { assertEquals(Method.GET.toString(), method) },
-                { assertEquals(Status.OK.code, status) }
-            )
-        }
-
-        with(events[1] as SlowHttpEvent) {
-            assertAll(
-                { assertEquals("/", uri) },
-                { assertEquals(Method.GET.toString(), method) },
-                { assertEquals(Status.OK.code, status) }
-            )
-        }
+        assertAll(
+            events.first() as HttpEvent,
+            { assertEquals("/", uri) },
+            { assertEquals(Method.GET.toString(), method) },
+            { assertEquals(Status.OK.code, status) }
+        )
+        assertAll(
+            events[1] as SlowHttpEvent,
+            { assertEquals("/", uri) },
+            { assertEquals(Method.GET.toString(), method) },
+            { assertEquals(Status.OK.code, status) }
+        )
     }
 }
