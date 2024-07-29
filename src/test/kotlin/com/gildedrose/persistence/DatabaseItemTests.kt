056a4102adf7e4f4c5b0908c923702cabee9631f
houlisha <16525973+HarisHoulis@users.noreply.github.com>
2024-07-29 08:08:41 +0200

    Run tests with the test database

--- src/test/kotlin/com/gildedrose/persistence/DatabaseItemTests.kt
+++ src/test/kotlin/com/gildedrose/persistence/DatabaseItemTests.kt
@@ -3,14 +3,14 @@ package com.gildedrose.com.gildedrose.persistence
 import org.jetbrains.exposed.sql.SchemaUtils
 import org.jetbrains.exposed.sql.transactions.transaction
 import org.junit.jupiter.api.BeforeEach
-import org.junit.jupiter.api.Disabled
+import org.junit.jupiter.api.condition.EnabledIfSystemProperty
 
-@Disabled
-internal class DatabaseItemTests : ItemsContract(DatabaseItems(db)) {
+@EnabledIfSystemProperty(named = "run-slow-tests", matches = "true")
+internal class DatabaseItemTests : ItemsContract(DatabaseItems(testDatabase)) {
 
     @BeforeEach
     fun setUp() {
-        transaction(db) {
+        transaction(testDatabase) {
             SchemaUtils.drop(DatabaseItems.Items)
             SchemaUtils.createMissingTablesAndColumns(DatabaseItems.Items)
         }
