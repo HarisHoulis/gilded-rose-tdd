2420d1ed3c9c969c7589cdb041dceb0ce2f6259a
houlisha <16525973+HarisHoulis@users.noreply.github.com>
2024-09-22 16:42:03 +0200

    chore: Disable db tests on CI

--- src/test/kotlin/com/gildedrose/persistence/DatabaseItemTests.kt
+++ src/test/kotlin/com/gildedrose/persistence/DatabaseItemTests.kt
@@ -4,7 +4,9 @@ import org.jetbrains.exposed.sql.SchemaUtils
 import org.jetbrains.exposed.sql.Transaction
 import org.jetbrains.exposed.sql.transactions.transaction
 import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.condition.EnabledIfSystemProperty
 
+@EnabledIfSystemProperty(named = "run-db-tests", matches = "true")
 internal class DatabaseItemTests : ItemsContract<Transaction>(
     items = DatabaseItems(testDatabase)
 ) {
